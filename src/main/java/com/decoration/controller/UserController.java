9b9a4304282f5114f6b8193852f2ea48f162c707
dongchaoxue <dongc.xue001@ehomepay.com.cn>
2020-08-15 20:36:22 +0800

    mvc接入

--- src/main/java/com/decoration/controller/UserController.java
+++ src/main/java/com/decoration/controller/UserController.java
@@ -2,24 +2,33 @@ package com.decoration.controller;
 
 import com.decoration.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
 
 /**
  * Created by dongchao on 2020/8/15 11:26
  */
-@RestController
+@Controller
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/test")
-    public String test(){
-        return "success";
+    @RequestMapping("/toAddUser")
+    public ModelAndView toAddUser(){
+        ModelAndView view = new ModelAndView();
+        view.setViewName("userAdd");
+        view.addObject("time", new Date());
+        return view;
     }
 
     @RequestMapping("/addUser")
+    @ResponseBody
     public String addUser(String name) {
 
         return String.valueOf(userService.addUser(name));
